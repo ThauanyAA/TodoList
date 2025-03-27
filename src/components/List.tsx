1dd39e0ce188bc58c2d8ea0eef5917b0272849d0
thauanyAA <thauanyaraujo5@gmail.com>
2025-03-26 22:34:06 -0300

    Implementing List of Tasks and adding empty state elements

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,10 @@
-import { Trash } from 'phosphor-react'
-import { Task } from '../types/task'
+import styles from './List.module.css'
+import { Task as TaskType } from '../types/task'
+import { Task } from './Task'
+import Clipboard from '../assets/Clipboard.svg'
 
 interface ListProps {
-  tasks: Task[],
+  tasks: TaskType[],
   onComplete: (task_id: string) => void,
   onDelete: (task_id: string) => void
 }
@@ -10,31 +12,36 @@ export function List({ tasks, onComplete, onDelete }: ListProps) {
   const completedTasks = tasks.reduce((ac, task) => task.isCompleted ? ac + 1 : ac, 0)
   
   return (
-    <div>
-      <header>
-        <div>
-          Tarefas criadas{' '}<span>{tasks.length}</span>
-        </div>
-        <div>
-          Concluídas <span>{completedTasks}</span>
-        </div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <span className={`${styles.stats} ${styles.blue}`}>
+          Tarefas criadas{' '}
+          <div className={styles.badge}>{tasks.length}</div>
+        </span>
+        <span className={`${styles.stats} ${styles.purple}`}>
+          Concluídas{' '}
+          <div className={styles.badge}>
+            {completedTasks ? `${completedTasks} de ${tasks.length}` : '0'}
+          </div>
+        </span>
       </header>
       <main>
-        {tasks.map(task => (
-          <div key={task.id}>
-            <input
-              type="checkbox"
-              id={task.id}
-              name={task.id}
-              value={task.id}
-              onChange={() => onComplete(task.id)}
-            />
-            {task.title}
-            <button onClick={() => onDelete(task.id)}>
-              <Trash />
-            </button>
+        {tasks.length ? tasks.map(task => (
+          <Task
+            key={task.id}
+            task={task}
+            onComplete={onComplete}
+            onDelete={onDelete}
+          />
+        )) : (
+          <div className={styles.emptyState}>
+            <img src={Clipboard} />
+            <p>
+              <strong>Você ainda não tem tarefas cadastradas.</strong>
+              <br />Crie tarefas e organize seus itens a fazer
+            </p>
           </div>
-        ))}
+        )}
       </main>
     </div>
   )
